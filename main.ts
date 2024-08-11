c68d11f410f2af8d5209bb3030027681b981a9fe
omeruysal81 <157880761+omeruysal81@users.noreply.github.com>
2024-08-11 17:07:49 +0300

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    sayi += 1
+    basic.showNumber(sayi)
+})
+input.onButtonPressed(Button.AB, function () {
+    sayi = 0
+    basic.showNumber(sayi)
+})
+input.onButtonPressed(Button.B, function () {
+    sayi += -1
+    basic.showNumber(sayi)
+})
+let sayi = 0
+sayi = 0
+basic.showNumber(sayi)
